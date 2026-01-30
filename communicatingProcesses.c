bd2e163a0583c640abe20f5392e5245ec549c108
halie-numinen <124086035+halie-numinen@users.noreply.github.com>
2026-01-30 18:26:00 -0500

    started with the code from earlier in lab and added on

--- communicatingProcesses.c
+++ communicatingProcesses.c
@@ -2,18 +2,37 @@
 #include <unistd.h>  
 #include <stdlib.h>  
 #include <signal.h> 
-void sigHandler (int); 
+
+void SIGUSR1 (int); 
+void SIGUSR2 (int)
+
+pid = fork();
+
 int main() {  
-    signal (SIGINT, sigHandler);  
     printf ("waiting...\n");  
+
+    signal (SIGINT, SIGUSR1);  
     pause();  
     return 0;  
 } 
-void sigHandler (int sigNum) {  
-    printf (" received an interrupt.\n");  
+void SIGUSR1 (int sigNum) {  
+    printf (" That's it, I'm shutting you down.\n");  
     // this is where shutdown code would be inserted  
     sleep (1);  
     printf ("time to exit\n");  
     exit(0);  
 } 
+
+ if(pid == 0)
+ { // Child process
+    pause(5)
+    write(fd[1], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
+    printf("Child wrote [%d]\n", signem);
+ }
+else
+ {
+    read(fd[0], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
+    printf("Parent received [%d] from child process\n", signum);
+ }
+
 
