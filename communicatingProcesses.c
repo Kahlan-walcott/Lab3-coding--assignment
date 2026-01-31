d1346ee3c44e26a5859ef728ef977c86381ff408
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-01-31 17:39:14 -0500

    I fixed most of the errors. The only ones I still have are from the Child process part, which we will fix later.

--- communicatingProcesses.c
+++ communicatingProcesses.c
@@ -2,37 +2,45 @@
 #include <unistd.h>  
 #include <stdlib.h>  
 #include <signal.h> 
+#include <sys/types.h>
+#include <string.h>
 
-void SIGUSR1 (int); 
-void SIGUSR2 (int)
+#define READ 0
+#define WRITE 1
 
-pid = fork();
+void sigHandler (int); 
 
 int main() {  
+    pid_t pid;
+    pid = fork();
+    signal (SIGINT, sigHandler);  
     printf ("waiting...\n");  
-
-    signal (SIGINT, SIGUSR1);  
     pause();  
+    if(pid == 0) { // Child process
+        pause(5);
+        write(fd[1], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
+        printf("Child wrote [%d]\n", signum);
+    }
+    else {
+        read(fd[0], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
+        printf("Parent received [%d] from child process\n", signum);
+    }
     return 0;  
 } 
-void SIGUSR1 (int sigNum) {  
-    printf (" That's it, I'm shutting you down.\n");  
-    // this is where shutdown code would be inserted  
-    sleep (1);  
-    printf ("time to exit\n");  
-    exit(0);  
+
+void sigHandler (int sigNum) {  
+    if (sigNum == SIGUSR1) {
+        printf("received a SIGUSR1 signal\n");
+    }
+    else if (sigNum == SIGUSR2) {
+        printf("received a SIGUSR2 signal\n");
+    }
+    else if (singNum == SIGINT) { // To find Ctrl c
+        printf (" That's it, I'm shutting you down.\n");  
+        // this is where shutdown code would be inserted  
+        sleep (1);  
+        printf ("time to exit\n");  
+        exit(0);  
+    }
 } 
-
- if(pid == 0)
- { // Child process
-    pause(5)
-    write(fd[1], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
-    printf("Child wrote [%d]\n", signem);
- }
-else
- {
-    read(fd[0], "SIGUSR1 OR 2 USE RANDOM FUNCTION", sizeof(int));
-    printf("Parent received [%d] from child process\n", signum);
- }
-
 
